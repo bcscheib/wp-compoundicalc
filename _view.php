f932affe2f16f22130bae19fd41999972f104b48
Ben Scheib <bcscheib@yahoo.com>
2017-06-13 12:11:13 -0400

    added avoid translation option

--- _view.php
+++ _view.php
@@ -1,103 +1,59 @@
-<?
-	if( !isset($principle) ) {
-		$principle = 0;
-	}	
-	if( !isset($apr) ) {
-		$apr = 0.00;
-	}
-	if( !isset($inflation_rate) ) {
-		$inflation_rate = 0.00;
-	}
-	if( !isset($deposit_op) ) {
-		$deposit_op = '+';
-	}
-	if( !isset($schedule) || !isset($schedule['years']) ) {
-		$schedule = array('years' => array());
-	}
-	
-	if( !isset($calculator_action_url) || !is_string($calculator_action_url) ) {
-		$calculator_action_url = '';
-	}
-	
-	if( !isset($calculator_action) || !is_string($calculator_action) ) {
-		$calculator_action = '';
-	}
-	
-	$form_prefix = !empty($calculator_action) && function_exists('the_ID') ? 'calc' . get_the_ID() : 'calc';
-	
-	if( !isset($texts) || !is_array($texts) ) {
-		$texts = array();
-	}
-	function wp_compoundicalc_label($name, $texts = array(), $default = '', $filter_function = null) {
-		$val = isset($texts[$name]) && is_string($texts[$name]) && !empty($texts[$name]) ? trim($texts[$name]) : $default;
-		
-		// sanitize these values 
-		$val = function_exists('sanitize_text_field') ? sanitize_text_field($val) : $val;
-		
-		if( function_exists($filter_function) ) {
-			$val = call_user_func_array($filter_function, array($val) );
-		}
-		echo $val;
-	}
-?>
 <div class="wp-compoundicalc-body">
 	<ul class="calculator_tabs">
-		<li class="calculator_tab_active"><a href="javascript:;"><?php wp_compoundicalc_label('tab_text', $texts, 'Regular Deposit / Withdrawal') ?></a></li>
+		<li class="calculator_tab_active"><a href="javascript:;"><?php CompoundiCalc_Core::labelize('tab_text') ?></a></li>
 	</ul>
-	<form method="POST" class="calculator_form" action="<?php echo  $calculator_action_url ?>">
+	<form method="POST" class="calculator_form" action="<?php echo admin_url('admin-post.php') ?>">
 		
-		<?php if( !empty($calculator_action) && function_exists('the_ID') ) { ?>
-			<input type="hidden" name="action" value="<?php echo $calculator_action ?>" />
-			<input type="hidden" name="origin" value="<?php the_ID(); ?>">
-		<? } ?>
+		<input type="hidden" name="action" value="<?php echo CompoundiCalc_Core::$action ?>" />
+		<input type="hidden" name="origin" value="<?php the_ID(); ?>">
 		
 		<div class="form_field">
-			<label><?php wp_compoundicalc_label('principle_text', $texts, 'Base Amount') ?></label>
-			<input type="number" name="<?php echo $form_prefix; ?>[principle]" value="<?php echo  $principle ?>" />
+			<label><?php CompoundiCalc_Core::labelize('principle_text') ?></label>
+			<input type="number" name="<?php echo CompoundiCalc_Core::$session_prefix; ?>[principle]" value="<?php echo  $principle ?>" />
 		</div>
 		<div class="form_field">
-			<label><?php wp_compoundicalc_label('apr_text', $texts, 'Base Amount') ?></label>
-			<input type="number" name="<?php echo $form_prefix; ?>[apr]" value="<?php echo  is_null($apr) ? null : $apr * 100 ?>" step="0.01" />
+			<label><?php CompoundiCalc_Core::labelize('apr_text') ?></label>
+			<input type="number" name="<?php echo CompoundiCalc_Core::$session_prefix; ?>[apr]" value="<?php echo $apr * 100 ?>" step="0.01" />
 			<span>%</span>
 		</div>
 		<div class="form_field">
-			<label><?php wp_compoundicalc_label('period_text', $texts, 'Calculation Period') ?></label>
-			<input type="number" name="<?php echo $form_prefix; ?>[years]" value="<?php echo  $years ?>" />
-			<span><?php wp_compoundicalc_label('years_plural_text', $texts, 'years', 'strtolower') ?></span>
+			<label><?php CompoundiCalc_Core::labelize('period_text') ?></label>
+			<input type="number" name="<?php echo CompoundiCalc_Core::$session_prefix; ?>[years]" value="<?php echo  $years ?>" />
+			<span><?php CompoundiCalc_Core::labelize('years_plural_text', 'years', 'strtolower') ?></span>
 		</div>
 		<div class="form_field">
-			<label class="not_required"><?php wp_compoundicalc_label('deposit_amount_text', $texts, 'Regular Monthly') ?></label>
-			<input type="number" name="<?php echo $form_prefix; ?>[deposit]" value="<?php echo  $deposit ?>" step="0.01" />
-			<select name="<?php echo $form_prefix; ?>[deposit_op]">
-				<option value="-"<?php echo  $deposit_op === '-' ? ' selected' : '' ?>><?php wp_compoundicalc_label('withdrawal_text', $texts, 'Withdrawal', 'ucwords') ?></option>
-				<option value=""<?php echo  $deposit_op !== '-' ? 'selected' : '' ?>><?php wp_compoundicalc_label('deposit_text', $texts, 'Deposit', 'ucwords') ?></option>
+			<label class="not_required"><?php CompoundiCalc_Core::labelize('deposit_amount_text') ?></label>
+			<input type="number" name="<?php echo CompoundiCalc_Core::$session_prefix; ?>[deposit]" value="<?php echo $deposit_amt ?>" step="1" />
+			<select name="<?php echo CompoundiCalc_Core::$session_prefix; ?>[deposit_op]">
+				<option value="-"<?php echo  $deposit_op === '-' ? ' selected' : '' ?>><?php CompoundiCalc_Core::labelize('withdrawal_text', 'ucwords') ?></option>
+				<option value=""<?php echo  $deposit_op !== '-' ? 'selected' : '' ?>><?php CompoundiCalc_Core::labelize('deposit_text', 'ucwords') ?></option>
 			</select>
 		</div>
 		<div class="form_field">
-			<label class="not_required"><?php wp_compoundicalc_label('inflation_rate_text', $texts, 'Inflation Rate?') ?></label>
-			<input type="number" name="<?php echo $form_prefix; ?>[inflation_rate]" value="<?php echo  is_null($inflation_rate) ? null : $inflation_rate * 100 ?>" step="0.01" />
+			<label class="not_required"><?php CompoundiCalc_Core::labelize('inflation_rate_text') ?></label>
+			<input type="number" name="<?php echo CompoundiCalc_Core::$session_prefix; ?>[inflation_rate]" value="<?php echo $inflation_rate * 100 ?>" step="1" />
 			<span class="not_required">%</span>
 		</div>
 		<div class="form_field">
 			<label>&nbsp;</label>
-			<input type="submit" name="submit" value="<?php wp_compoundicalc_label('calculate_text', $texts, 'Calculate') ?>" />
+			<input type="submit" name="submit" value="<?php CompoundiCalc_Core::labelize('calculate_text') ?>" />
 		</div>
 	</form>
 	
 	<div class="calculator_results<?php echo  is_null($principle) ? ' calculator_hidden' : '' ?>">
 	    <ul class="calculator_tabs calculator_body_tabs">
-	    	<li class="results_tab calculator_tab_active"><a href="javascript:;"><?php wp_compoundicalc_label('results_tab_text', $texts, 'Calculation Results') ?></a></li>
-	    	<li class="graphs_tab"><a href="javascript:;"><?php wp_compoundicalc_label('graphs_tab_text', $texts, 'Graphs of Results') ?></a></li>
+	    	<li class="results_tab calculator_tab_active"><a href="javascript:;"><?php CompoundiCalc_Core::labelize('results_tab_text') ?></a></li>
+	    	<li class="graphs_tab"><a href="javascript:;"><?php CompoundiCalc_Core::labelize('graphs_tab_text') ?></a></li>
 	    </ul>
 		<table>
 			<thead>
 				<tr>
-					<td class="calculator_highlight"><?php wp_compoundicalc_label('years_singular_text', $texts, 'Year', 'ucwords') ?></td>
-					<td><?php wp_compoundicalc_label('years_singular_text', $texts, 'Year', 'ucwords') ?> <?php   $deposit_op === '-' ?  wp_compoundicalc_label('withdrawal_text', $texts, 'Withdrawal', 'ucwords') : wp_compoundicalc_label('deposit_text', $texts, 'Deposit', 'ucwords') ?></td>
-					<td><?php wp_compoundicalc_label('years_singular_text', $texts, 'Year', 'ucwords') ?> <?php wp_compoundicalc_label('interest_text', $texts, 'Interest', 'ucwords') ?></td>
-					<td><?php wp_compoundicalc_label('total_text', $texts, 'Total', 'ucwords') ?> <?php $deposit_op === '-' ? wp_compoundicalc_label('withdrawal_text', $texts, 'Withdrawal', 'ucwords') : wp_compoundicalc_label('deposit_text', $texts, 'Deposit', 'ucwords') ?></td>
-					<td><?php wp_compoundicalc_label('total_text', $texts, 'Total', 'ucwords') ?> <?php wp_compoundicalc_label('interest_text', $texts, 'Interest', 'ucwords') ?></td>
-					<td><?php wp_compoundicalc_label('balance_text', $texts, 'Balance', 'ucwords') ?></td>
+					<td class="calculator_highlight"><?php CompoundiCalc_Core::labelize('years_singular_text', 'ucwords') ?></td>
+					<td><?php CompoundiCalc_Core::labelize('years_singular_text', 'ucwords') ?> <?php   $deposit_op === '-' ?  CompoundiCalc_Core::labelize('withdrawal_text', 'ucwords') : CompoundiCalc_Core::labelize('deposit_text', 'ucwords') ?></td>
+					<td><?php CompoundiCalc_Core::labelize('years_singular_text', 'ucwords') ?> <?php CompoundiCalc_Core::labelize('interest_text', 'Interest', 'ucwords') ?></td>
+					<td><?php CompoundiCalc_Core::labelize('total_text', 'ucwords') ?> <?php $deposit_op === '-' ? CompoundiCalc_Core::labelize('withdrawal_text', 'Withdrawal', 'ucwords') : CompoundiCalc_Core::labelize('deposit_text', 'ucwords') ?></td>
+					<td><?php CompoundiCalc_Core::labelize('total_text', 'ucwords') ?> <?php CompoundiCalc_Core::labelize('interest_text', 'ucwords') ?></td>
+					<td><?php CompoundiCalc_Core::labelize('balance_text', 'ucwords') ?></td>
 				</tr>
 			</thead>
 			<tbody>
@@ -121,20 +77,20 @@
 		</table>
 		
 		<script>
-			wpcompoundicalc.graph1.title = '<?php wp_compoundicalc_label('graph1_title_text', $texts, 'Graph 1 - Balance (Compounded Monthly)') ?>';
-			wpcompoundicalc.graph1.balance = '<?php wp_compoundicalc_label('balance_text', $texts, 'balance', 'strtolower') ?>';
-			wpcompoundicalc.graph1.years = '<?php wp_compoundicalc_label('years_plural_text', $texts, 'years', 'strtolower') ?>';
+			wpcompoundicalc.graph1.title = '<?php CompoundiCalc_Core::labelize('graph1_title_text') ?>';
+			wpcompoundicalc.graph1.balance = '<?php CompoundiCalc_Core::labelize('balance_text', 'strtolower') ?>';
+			wpcompoundicalc.graph1.years = '<?php CompoundiCalc_Core::labelize('years_plural_text', 'strtolower') ?>';
 			
-			wpcompoundicalc.graph1.data = [ ['<?php wp_compoundicalc_label('years_singular_text', $texts, 'Year', 'ucwords') ?>', '<?php wp_compoundicalc_label('balance_text', $texts, 'Balance', 'ucwords') ?>'],
+			wpcompoundicalc.graph1.data = [ ['<?php CompoundiCalc_Core::labelize('years_singular_text', 'ucwords') ?>', '<?php CompoundiCalc_Core::labelize('balance_text', 'Balance', 'ucwords') ?>'],
 			  <? foreach($schedule['years'] as $year => $year_schedule) { ?>
 			  	['<?php echo  $year + 1 ?>', <?php echo  $year_schedule['balance'] ?> ],
 			  <? } ?>
 	          
 	          ];
-	          wpcompoundicalc.graph2.title = '<?php wp_compoundicalc_label('graph2_title_text', $texts, 'Graph 2 - Total Interest (Compounded Monthly)') ?>';
-	          wpcompoundicalc.graph2.years = '<?php wp_compoundicalc_label('years_plural_text', $texts, 'years', 'strtolower') ?>';
-	          wpcompoundicalc.graph2.interest = '<?php wp_compoundicalc_label('interest_text', $texts, 'interest', 'strtolower') ?>';
-	        wpcompoundicalc.graph2.data = [ ['<?php wp_compoundicalc_label('years_singular_text', $texts, 'Year', 'ucwords') ?>', '<?php wp_compoundicalc_label('total_text', $texts, 'Total', 'ucwords') ?> <?php wp_compoundicalc_label('interest_text', $texts, 'Interest', 'ucwords') ?>'],
+	          wpcompoundicalc.graph2.title = '<?php CompoundiCalc_Core::labelize('graph2_title_text') ?>';
+	          wpcompoundicalc.graph2.years = '<?php CompoundiCalc_Core::labelize('years_plural_text', 'strtolower') ?>';
+	          wpcompoundicalc.graph2.interest = '<?php CompoundiCalc_Core::labelize('interest_text', 'strtolower') ?>';
+	        wpcompoundicalc.graph2.data = [ ['<?php CompoundiCalc_Core::labelize('years_singular_text', 'ucwords') ?>', '<?php CompoundiCalc_Core::labelize('total_text', 'ucwords') ?> <?php CompoundiCalc_Core::labelize('interest_text', 'ucwords') ?>'],
 			  <? foreach($schedule['years'] as $year => $year_schedule) { ?>
 			  	['<?php echo  $year + 1 ?>', <?php echo  $year_schedule['interest'] ?> ],
 			  <? } ?>
--- wp-compoundicalc.php
+++ wp-compoundicalc.php
@@ -10,12 +10,10 @@ Text Domain: wp-compoundicalc
 
 require_once dirname( __FILE__ ) . '/scb/load.php';
 
-function _pagenavi_init() {
+function _compoundicalc_init() {
 	load_plugin_textdomain( 'wp-compoundicalc' );
-
-	require_once dirname( __FILE__ ) . '/core.php';
-
-	$options = new scbOptions( 'compoundicalc_options', __FILE__, array(
+	
+	$translations = new scbOptions( 'compoundicalc_options', __FILE__, array(
 		/* misc values */
 		'withdrawal_text'   => 'Withdrawal',
 		'deposit_text'   => 'Deposit',
@@ -41,13 +39,212 @@ function _pagenavi_init() {
 		'graph1_title_text'   => 'Graph 1 - Balance (Compounded Monthly)',
 		'graph2_title_text'   => 'Graph 2 - Total Interest (Compounded Monthly)',
 	) );
-    
-	CompoundiCalc_Core::init( $options );
+	
+	$default_args = array(
+		'before' => '',
+		'after' => '',
+		'wrapper_tag' => 'div',
+		'wrapper_class' => 'wp-compoundicalc',
+		'query' => $GLOBALS['wp_query'],
+		'type' => 'posts',
+		'principle' => 25000.00,
+		'apr' => 0.03,
+		'inflation_rate' => 0,
+		'years' => 1,
+		'deposit' => 0.0,
+		'translate' => true
+	);
+
+	add_action('init', function(){
+		if( !session_id() )
+	  		session_start();
+		}
+	);
+	add_action( 'admin_post_wp-compoundicalc', 'wp_compoundicalc_post' );
+	add_action( 'admin_post_nopriv_wp-compoundicalc', 'wp_compoundicalc_post' );
+		
+	function wp_compoundicalc_post() {
+		$page_id = isset($_REQUEST['origin']) ? intval($_REQUEST['origin']) : null;
+		
+		if( isset($_POST['calc' . $page_id]) ) {
+			$post_prefix_text = 'calc' . $page_id;
+			$post_prefix = $_POST[$post_prefix_text];
+			
+			if( isset($post_prefix['apr']) ) {
+				$_SESSION[$post_prefix_text . '_apr'] = floatval($post_prefix['apr']) / 100;
+			}
+			
+			if( isset($post_prefix['principle']) ) {
+				$_SESSION[$post_prefix_text . '_principle'] = floatval($post_prefix['principle']);
+			}
+		
+			if( isset($post_prefix['deposit']) ) {
+				$_SESSION[$post_prefix_text . '_deposit'] = floatval($post_prefix['deposit']);
+			}
+		
+			if( isset($post_prefix['years']) ) {
+				$_SESSION[$post_prefix_text . '_years'] = intval($post_prefix['years']);
+			}
+		
+			if( isset($post_prefix['inflation_rate'])  ) {
+				$_SESSION[$post_prefix_text . '_inflation_rate'] = floatval($post_prefix['inflation_rate']) / 100;
+			}
+			
+			if( isset($post_prefix['deposit_op']) ) {
+				$_SESSION[$post_prefix_text . '_deposit_op'] = $post_prefix['deposit_op'];
+			}
+		} 
+		$url = !is_null($page_id) ? get_permalink($page_id) : '/';
+		wp_redirect($url);
+	    exit();
+	}
+
+	function wp_compoundicalc( $args = array() ) {
+		require_once( __DIR__ . '/lib/calculator.php' );
+		
+		CompoundiCalc_Core::set_prop('args', CompoundiCalc_Core::merge_args($args) );
+		CompoundiCalc_Core::set_prop('session_prefix', 'calc' . get_the_id());
+		CompoundiCalc_Core::set_prop('action', 'wp-compoundicalc');
+		
+		extract(CompoundiCalc_Core::$args, EXTR_SKIP );
+		
+		$deposit_amt = CompoundiCalc_Core::floatize_prop('deposit');   
+		$apr = CompoundiCalc_Core::floatize_prop('apr');   
+		$inflation_rate = CompoundiCalc_Core::floatize_prop('inflation_rate');   
+		$principle = CompoundiCalc_Core::intize_prop('principle');   
+		$years = CompoundiCalc_Core::intize_prop('years');   
+		
+		$deposit_op = isset($_SESSION[$session_prefix . '_deposit_op']) && $_SESSION[CompoundiCalc_Core::$session_prefix . '_deposit_op'] === '-' ? $_SESSION[CompoundiCalc_Core::$session_prefix . '_deposit_op'] : '+';
+		
+		if( $deposit_op === '-' ) {
+			$deposit_amt = $deposit_amt * -1;
+		}
+		
+		CompoundiCalc_Core::reset_session();
+		
+		$instance = new Calculator();
+		$schedule = $instance->calculate_schedule($deposit_amt, $principle, $apr, 12, $years, $inflation_rate);
+		    
+		$tmpl = CompoundiCalc_Core::start_template();
+		require_once(__DIR__ . '/_view.php');
+		CompoundiCalc_Core::end_template();
+		
+		
+		$out = $before . "<" . $wrapper_tag . " class='" . $wrapper_class . "'>\n" . $tmpl->body . "\n</" . $wrapper_tag . ">" . $after;
+	
+		return apply_filters( 'wp_compoundicalc', $out );
+	}
+
+	class CompoundiCalc_Core {
+		
+		public static $action;
+		public static $args;
+		public static $default_args;
+		public static $session_prefix;
+		public static $translations;
+		
+		static function set_prop($name, $val) {
+			$class_name = get_called_class();
+			$class = new ReflectionClass($class_name);
+			$class->setStaticPropertyValue($name, $val);
+		}
+	
+		static function init( $translations = array(), $default_args = array()) {
+			static::$translations = $translations;
+			static::$default_args = $default_args;
+			static::$args = $default_args;
+			
+			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'stylesheets' ) );
+			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'scripts' ) );
+			add_shortcode('compound_calculator', 'wp_compoundicalc');
+		}
+		
+		static function merge_args($args) {
+			return wp_parse_args( $args, static::$default_args );
+		}
+		
+		static function reset_session() {
+			foreach($_SESSION as $key => $val) {
+				if( 0 == strpos($key, $_SESSION[static::$session_prefix]) ) {
+					unset($_SESSION[$key]);
+				}
+			}
+		}
+	
+		static function stylesheets() {
+	
+			if ( @file_exists( get_stylesheet_directory() . '/wp-compoundicalc-css.css' ) )
+				$css_file = get_stylesheet_directory_uri() . '/wp-compoundicalc-css.css';
+			elseif ( @file_exists( get_template_directory() . '/wp-compoundicalc-css.css' ) )
+				$css_file = get_template_directory_uri() . '/wp-compoundicalc-css.css';
+			else
+				$css_file = plugins_url( 'wp-compoundicalc-css.css', __FILE__ );
+	
+			wp_enqueue_style( 'wp-compoundicalc', $css_file, false, '2.70' );
+		}
+		
+		static function scripts() {
+	
+			if ( @file_exists( get_stylesheet_directory() . '/wp-compoundicalc-js.js' ) )
+				$js_file = get_stylesheet_directory_uri() . '/wp-compoundicalc-js.js';
+			elseif ( @file_exists( get_template_directory() . '/wp-compoundicalc-js.js' ) )
+				$js_file = get_template_directory_uri() . '/wp-compoundicalc-js.js';
+			else
+				$js_file = plugins_url( '/wp-compoundicalc-js.js', __FILE__ );
+	
+			wp_enqueue_script( 'gcharts', 'https://www.gstatic.com/charts/loader.js', array('jquery'), '3.3.5', true );
+			wp_enqueue_script( 'wp-compoundicalc', $js_file, array('gcharts'), '2.70' );
+		}
+		
+		static function labelize($name, $filter_function = null) {
+			$dont_translate = static::$args['translate'] === false || static::$args['translate'] === "false";
+			$val = $dont_translate ? static::$translations->get_defaults($name) : static::$translations->get($name);
+			
+			$val = function_exists('sanitize_text_field') ? trim(sanitize_text_field($val)) : trim($val); // sanitize these values 
+			
+			if( function_exists($filter_function) ) {
+				$val = call_user_func_array($filter_function, array($val) );
+			}
+			echo $val;
+		}
+		
+		static function floatize_prop($prop) {
+			return isset($_SESSION[static::$session_prefix . '_' . $prop]) ? abs(floatval($_SESSION[static::$session_prefix . '_' . $prop])) : abs(floatval(static::$args[$prop]));
+		}
+		
+		static function intize_prop($prop) {
+			return isset($_SESSION[static::$session_prefix . '_' . $prop]) ? intval($_SESSION[static::$session_prefix . '_' . $prop]) : intval(static::$args[$prop]);
+		}
+		
+		static function start_template() {
+			$tmpl = new stdClass();
+			$tmpl->body = '';
+			ob_start(function($buffer) use ($tmpl) {
+				$tmpl->body = $buffer;
+				return $buffer;
+			});
+			return $tmpl;
+		}
+		
+		static function end_template() {
+			ob_end_clean();
+		}	
+		
+		static function evaluate_template($tmpl, $key_values = array() ) {
+			$body = $tmpl->body;
+			foreach( $key_values as $key => $value ) {
+				$body = str_replace("#$key#", $value, $body);
+			}
+			return $body;
+		}
+	}
+
+	CompoundiCalc_Core::init( $translations, $default_args );
 
 	if ( is_admin() ) {
 		require_once dirname( __FILE__ ) . '/admin.php';
 		new CompoundCalculator_Options_Page( __FILE__, $options );
 	}
 }
-scb_init( '_pagenavi_init' );
+scb_init( '_compoundicalc_init' );
 
