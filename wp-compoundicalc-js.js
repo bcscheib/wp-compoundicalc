1a4c5d4764b83e08a707b3777b764871c0a9b206
Ben Scheib <bcscheib@yahoo.com>
2017-06-13 02:58:02 -0400

    added css and js

--- wp-compoundicalc-js.js
+++ wp-compoundicalc-js.js
@@ -1,1 +1,82 @@
-/Users/BenS/workspace/compound_interest_calculator_plugin/js/charts.js
+var wpcompoundicalc = {
+	graph1: {},
+	graph2: {}
+};
+
+(function(){
+  	if(typeof jQuery !=='undefined') {
+    	jQuery(document).ready(function($){
+			var results_tab = $('.results_tab'),
+		    graphs_tab = $('.graphs_tab'),
+			    results_div = $('.calculator_results table'),
+			    graphs_div = $('.calculator_graphs');
+			    
+			$('.calculator_tabs li').click(function(){
+				$(this).parent('ul').find('li').removeClass('calculator_tab_active');
+				$(this).addClass('calculator_tab_active');
+			});
+			
+			results_tab.on('click', function(){
+				results_div.show();
+				graphs_div.addClass('calculator_hidden');
+			});
+			
+			graphs_tab.on('click', function(){
+				results_div.hide();
+				graphs_div.removeClass('calculator_hidden');
+			});
+		});
+	} else {
+		throw new Error('jQuery is required for the Compound Interest Calculator Plugin.')
+	}
+	          
+	if( (typeof google !=='undefined') && (typeof google.charts !=='undefined') ) {
+	  google.charts.load('current', {'packages':['corechart']});
+	  google.charts.setOnLoadCallback( function  () {
+	    var options = {
+	      title: wpcompoundicalc.graph1.title,
+	      legend: { position: 'bottom' },
+	      series: {
+	        0: { targetAxisIndex: 0, color: '#000099' },
+	      },
+	    vAxes: {
+	      0: {title: wpcompoundicalc.graph1.balance,  titleTextStyle: {color: '#008000'}, format: 'currency'},
+	    },
+	     hAxes: {
+	      0: {title: wpcompoundicalc.graph1.years,  titleTextStyle: {color: '#008000'}},
+	    },
+	    titleTextStyle: {
+		    color: '#000099'
+		}
+	    };
+	
+	    var chart1 = new google.visualization.LineChart(document.getElementById('calculator_balance_chart'));
+	
+	    chart1.draw(google.visualization.arrayToDataTable(wpcompoundicalc.graph1.data), options);
+	    
+	    var options2 = {
+	      title: wpcompoundicalc.graph2.title,
+	      legend: { position: 'bottom' },
+	      series: {
+	        0: { targetAxisIndex: 0, color: '#ff0000' },
+	      },
+	      vAxes: {
+	      0: {title: wpcompoundicalc.graph2.interest,  titleTextStyle: {color: '#008000'}, format: 'currency'},
+	    },
+	     hAxes: {
+	      0: {title: wpcompoundicalc.graph2.years,  titleTextStyle: {color: '#008000'}},
+	    },
+	    titleTextStyle: {
+		    color: '#ff0000'
+		}
+	    };
+	    
+	    var chart2 = new google.visualization.LineChart(document.getElementById('calculator_interest_chart'));
+	
+	    chart2.draw(google.visualization.arrayToDataTable(wpcompoundicalc.graph2.data), options2);
+	  });
+	 
+	} else {
+	throw new Error('Google charts is required.')
+}
+})();
